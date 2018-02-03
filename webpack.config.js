89d8d63e44e6c6d76029528bf86e14d4a48599ec
admin <visualjupiter@gmail.com>
2018-02-03 21:49:16 +0900

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,17 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
   devServer: {
     open: true,
     contentBase: path.join(__dirname, 'src'),
